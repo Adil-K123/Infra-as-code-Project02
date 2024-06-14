e3afe3f04a66384843a5929ea2486e0949384360
adilk <adilk@ubuntu-main.myguest.virtualbox.org>
2024-06-14 19:07:41 +0530

    added dev directory and modules,updated workflow to do tf operations in dev directory

---
+++ modules/ec2/vars.tf
@@ -0,0 +1,15 @@
+variable "environment" {
+  type = string
+  default = ""
+}
+
+variable "mainvpcid" {
+  type = string
+  default = ""
+}
+
+variable "PublicSubnet1_id" {
+   type = string
+   default = ""
+}
+
---
+++ modules/rds/vars.tf
@@ -0,0 +1,35 @@
+variable "environment" {
+  type = string
+  default = ""
+}
+
+variable "mainvpcid" {
+  type = string
+  default = ""
+}
+
+variable "webserverSGid" {
+  type = string
+  default = ""
+}
+
+variable "allocated_storage" {
+  type = number
+  default = 20
+}
+
+variable "dbusername" {
+  type = string
+  default = ""
+}
+
+variable "dbpassword" {
+  type = string
+  sensitive = true
+  default = ""
+}
+
+variable "dbsubnetgroupname" {
+  type = string
+  default = ""
+}
