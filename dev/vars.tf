fe689abc99fdd9a71b9be98994af2ef47204efe6
adilk <adilk@ubuntu-main.myguest.virtualbox.org>
2024-06-19 18:38:53 +0530

    testing secret mgmt issue 1

--- dev/vars.tf
+++ dev/vars.tf
@@ -4,13 +4,11 @@ variable "environment" {
 }
 
 variable "dbusername" {
-  type = string
   default = ""
   sensitive = true
 }
 
 variable "dbpassword" {
-  type = string
   default = ""
   sensitive = true
 }
