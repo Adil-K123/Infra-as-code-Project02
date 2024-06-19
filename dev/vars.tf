a3f4f1758aec145981f93382d8cfe97f26cbd173
adilk <adilk@ubuntu-main.myguest.virtualbox.org>
2024-06-19 18:23:37 +0530

    commented out variable definition to test

--- dev/vars.tf
+++ dev/vars.tf
@@ -3,14 +3,14 @@ variable "environment" {
   default = "dev"
 }
 
-variable "dbusername" {
-  type = string
-  default = ""
-  sensitive = true
-}
+# variable "dbusername" {
+#   type = string
+#   default = ""
+#   sensitive = true
+# }
 
-variable "dbpassword" {
-  type = string
-  default = ""
-  sensitive = true
-}
+# variable "dbpassword" {
+#   type = string
+#   default = ""
+#   sensitive = true
+# }
