9a458a3ef05e440e0dd2baedec4f863c0f53cf6e
adilk <adilk@ubuntu-main.myguest.virtualbox.org>
2024-06-21 20:23:01 +0530

    corrected a typo in inventory module

--- modules/ec2/inventory.tf
+++ modules/ec2/inventory.tf
@@ -1,7 +1,7 @@
 resource "local_file" "inventory" {
     content = templatefile("${path.module}/templates/inventory.tpl",
      {
-        webServers = aws_instance.WebServer.*.associate_public_ip_address
+        webServers = aws_instance.Webserver.*.associate_public_ip_address
      }
     )
     filename = "${path.module}/inventory/hosts.cfg"
