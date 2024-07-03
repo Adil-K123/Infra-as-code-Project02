4d77453d68b6e3bbf8d62618571dfbfb2cd73c06
adilk <adilk@ubuntu-main.myguest.virtualbox.org>
2024-07-03 18:36:13 +0530

    added ports for swarm in security group

--- modules/ec2/instance.tf
+++ modules/ec2/instance.tf
@@ -48,6 +48,44 @@ resource "aws_security_group" "Project02WebServer_SG" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    description      = "swarm_manager"
+    from_port        = 2377
+    to_port          = 2377
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "swarm_overlay_node_discovery"
+    from_port        = 7946
+    to_port          = 7946
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  ingress {
+    description      = "swarm_overlay_node_discovery"
+    from_port        = 7946
+    to_port          = 7946
+    protocol         = "udp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  ingress {
+    description      = "swarm_overlay_traffic"
+    from_port        = 4789
+    to_port          = 4789
+    protocol         = "udp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
