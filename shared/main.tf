3487a811411901566f31afe4339ea208baaa3829
adilk <adilk@ubuntu-main.myguest.virtualbox.org>
2024-07-05 18:48:13 +0530

    destroy resources

--- shared/main.tf
+++ shared/main.tf
@@ -1,10 +1,10 @@
-module "shared_ecr" {
-    source = "./modules/ecr"
-}
+# module "shared_ecr" {
+#     source = "./modules/ecr"
+# }
 
-module "iam_roles" {
-    source = "./modules/iam"
-}
+# module "iam_roles" {
+#     source = "./modules/iam"
+# }
 
 # module "shared_vpc" {
 #     source = "./modules/vpc"
